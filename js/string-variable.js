bf45aede4daa0cea1c2419ace2b3b3438a789210
muratkilic1978 <muratkilic1978@gmail.com>
2020-01-28 17:31:46 +0100

    script.js was updated with solution

---
+++ js/string-variable.js
@@ -0,0 +1,9 @@
+let userName, message;
+const nameElem = document.querySelector("#name");
+const messageElem = document.querySelector("#note");
+userName = "Ali Baba";
+message = "See our upcoming range";
+
+nameElem.textContent = userName;
+
+messageElem.textContent = message;
